30daabf10967a8071ec8dbddc2cb03c4d732e944
swatsengels1 <70349220+swatsengels1@users.noreply.github.com>
2024-04-13 13:33:25 +0200

    added the stuff to the notebook

--- src/models/decision_tree_model.py
+++ src/models/decision_tree_model.py
@@ -4,11 +4,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 #Loading in the data - currently just the toy data from Amir
-data = pd.read_csv('/Users/sophieengels/DMT_Assignments/data/preprocessed/toy_data.csv')
+data = pd.read_csv('../../data/preprocessed/toy_data.csv')
 data['mood'] = data['mood'].round() #Can't be continuous
+data['time'] = (pd.to_datetime(data['time']) - pd.Timestamp('1970-01-01')) // pd.Timedelta('1s')
 
 #Separating the feature data and the target data
-X = data.drop(columns=['id', 'time', 'mood'])  
+X = data.drop(columns=['mood'])  
 y = data['mood']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -17,6 +18,7 @@ rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_classifier.fit(X_train, y_train)
 
 #Trying out the test set
+print(X_test.shape)
 y_pred = rf_classifier.predict(X_test)
 
 #Model Evaluation
