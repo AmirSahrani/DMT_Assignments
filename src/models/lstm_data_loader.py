2ad3fca8a469936fb078bd28a8f80a60d784c9a9
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2024-04-16 09:20:52 +0200

    add hyperparameter tuning

--- src/models/lstm_data_loader.py
+++ src/models/lstm_data_loader.py
@@ -1,41 +1,37 @@
 from torch.utils.data import Dataset
 import pandas as pd
-import numpy as np
 import torch
-import pdb
 
 class MoodDataset(Dataset):
     def __init__(self, csv_file, included_columns, sequence_length):
+        # Load the data
         self.data = pd.read_csv(csv_file)
 
-        # Ensure that the 'mood' column is in the included columns for labels
-        included_columns.append('mood')
-        
         # Filter the DataFrame to only include specified columns
-        self.data = self.data[included_columns]
+        self.data = self.data[included_columns + ['mood']]  # Include 'mood' explicitly
 
         # Extracting labels ('mood' column) and converting them to long type for classification
-        self.labels = self.data['mood'].astype(int).values
+        self.labels = torch.tensor(self.data['mood'].values).long()
+
+        # Drop the 'mood' column from features to ensure it's not included in the input features
+        self.features = self.data.drop(columns=['mood']).values
         
-        # Drop the 'mood' column from features
-        self.features = self.data.drop(columns=['mood'], errors='ignore').values
-        
-        # Create sequences
+        # Create sequences of features and labels
         self.feature_sequences, self.label_sequences = self._create_sequences(sequence_length)
 
     def _create_sequences(self, sequence_length):
-        # Create sequences of features and corresponding labels
+        # Initialize lists to hold sequences of features and labels
         feature_sequences, label_sequences = [], []
-        for i in range(len(self.features) - sequence_length + 1):
+        for i in range(len(self.features) - sequence_length):
             feature_sequences.append(self.features[i:i + sequence_length])
-            label_sequences.append(self.labels[i + sequence_length - 1])  # Label for the last item in the sequence
-        return np.array(feature_sequences), np.array(label_sequences)
+            label_sequences.append(self.labels[i + sequence_length])
+        return feature_sequences, label_sequences
 
     def __len__(self):
+        # Return the number of sequences available
         return len(self.feature_sequences)
 
     def __getitem__(self, index):
-        # Return a single sequence of features and the corresponding label
-        return torch.tensor(self.feature_sequences[index], dtype=torch.float), torch.tensor(self.label_sequences[index], dtype=torch.long)
-
+        # Return a tuple of feature sequence and corresponding label for the given index
+        return torch.tensor(self.feature_sequences[index], dtype=torch.float), self.label_sequences[index]
 
