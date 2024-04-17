02a4d73ab16e1221ce0ab9d44722423bff720e22
AmirSahrani <amirsahrani1@gmail.com>
2024-04-17 16:42:56 +0200

    ridge and shift

--- src/models/gaussian_process.py
+++ src/models/gaussian_process.py
@@ -1,23 +1,63 @@
 from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.linear_model import Ridge
+from sklearn.feature_selection import SequentialFeatureSelector
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 
+def data_numeric(data):
+    non_numeric_columns = [col for col in data.columns if data[col].dtype == 'object' or data[col].dtype.name == 'category']
+    return data.drop(columns=non_numeric_columns)  # Drop non-numeric columns
+
+    # Perform one-hot encoding and concatenate all at once
+
+
+def shift_moods(df):
+    # Shift the 'mood' column within each group defined by 'id'
+    df['next_mood'] = df.groupby('id')['mood'].shift(-1)
+    # Drop the last entry of each group because its 'next_mood' will be NaN
+    df = df.dropna(subset=['next_mood'])
+
+    # make subset dtype int inplace
+    df['next_mood'] = df['next_mood'].astype(int)
+    return df
+
+
+def feature_selection(X, y):
+    # Create the Gaussian Process Regressor
+    gp = GaussianProcessRegressor()
+
+    sfs = SequentialFeatureSelector(gp, n_features_to_select=5, direction='backward')
+    sfs.fit(X, y)
+    columns = X.columns[sfs.get_support()]
+    return columns
+
+
 def main():
-    data = pd.read_csv('../../data/preprocessed/engineered_data.csv')
-    mood = data['mood']
-    print(len(data))
-    # change id to a categorical variable
-    data['id'] = data['id'].astype('category')
+    time_columns = ['time', 'hour', 'day_of_week', 'day_of_month', 'month', 'hour_sin', 'hour_cos']
+    # Load and preprocess data
+    data = pd.read_csv('../../data/preprocessed/train_final.csv')
+    # data = data.drop(columns=time_columns, axis=1)
+    data = shift_moods(data)
+    data_numerical = data_numeric(data)
 
-    # drop columns that are not numeric
-    num_cols = data.select_dtypes(include=['float64', 'int64']).columns
-    data_numerical = data[num_cols]
+    test_data = pd.read_csv('../../data/preprocessed/test_final.csv')
+    # test_data = test_data.drop(columns=time_columns, axis=1)
+    test_data = shift_moods(test_data)
+    test_data_numerical = data_numeric(test_data)
+    print(len(data_numerical.columns))
 
-    data_numerical = data_numerical.drop(columns=['mood'])
-    X_train, X_test, y_train, y_test = train_test_split(data_numerical.values, mood.values, test_size=0.2, random_state=52)
+    # Define features and target
+    y_train = data_numerical.pop('next_mood').values
+    X_train = data_numerical.values
+    y_test = test_data_numerical.pop('next_mood').values
+    X_test = test_data_numerical.values
+
+
+    # output shapes of the split data
+    print(f"X_train shape: {X_train.shape}, X_test shape: {X_test.shape}, y_train shape: {y_train.shape}, y_test shape: {y_test.shape}")
 
     print(f'''
     X_train shape {X_train.shape},
@@ -37,12 +77,16 @@ def main():
     -------------------
     ''')
 
-    gp = GaussianProcessRegressor()
+    # Train Gaussian Process Regressor on selected features
+    # gp = GaussianProcessRegressor()
+    # Using ridge regression
+    gp = Ridge()
     gp.fit(X_train, y_train)
-    print(f'Train Score: {gp.score(X_train, y_train):.2f}')
-    print(f'Test Score: {gp.score(X_test, y_test):.2f}')
 
-    y_pred, y_err = gp.predict(X_test, return_std=True)
+    print(f'Train Score: {gp.score(X_train, y_train):.2f}')
+
+    y_pred = gp.predict(X_test)
+    y_pred = np.round(y_pred)
 
     print(f'''
     -------------------
@@ -52,7 +96,13 @@ def main():
     -------------------
     ''')
 
-    plt.errorbar(np.arange(len(y_test)), y_pred, yerr=y_err, fmt='-', label='Predicted')
+    mse = np.mean((y_pred - y_test) ** 2)
+    mae = np.mean(np.abs(y_pred - y_test))
+
+    print(f'Mean Squared Error: {mse:.2f}')
+    print(f'Mean Absolute Error: {mae:.2f}')
+
+    plt.plot(np.arange(len(y_pred)), y_pred, label='Predicted')
     plt.plot(np.arange(len(y_test)), y_test, label='True')
     plt.ylim(0, 10)
     plt.legend()
