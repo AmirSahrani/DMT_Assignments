4ff6502f6301f93e1ad9a1c47e1331cc8ee4aeed
AmirSahrani <amirsahrani1@gmail.com>
2024-04-17 17:01:31 +0200

    trees

--- src/models/decision_tree_model.py
+++ src/models/decision_tree_model.py
@@ -1,23 +1,35 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_class_weight
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, log_loss, make_scorer
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import randint
 import numpy as np
 import matplotlib.pyplot as plt
+from gaussian_process import shift_moods
 
-train_data = pd.read_csv('../../data/preprocessed/train_final.csv')
-test_data = pd.read_csv('../../data/preprocessed/test_final.csv')
+train_data = shift_moods(pd.read_csv('../../data/preprocessed/train_final.csv'))
+test_data = shift_moods(pd.read_csv('../../data/preprocessed/test_final.csv'))
 
-X_train = train_data[['id','activity','call','circumplex.arousal','circumplex.valence']]
+X_train = train_data[['id', 'activity', 'call', 'circumplex.arousal', 'circumplex.valence']]
 X_train['id'] = X_train['id'].str[-2:]
 
-X_test = test_data[['id','activity','call','circumplex.arousal','circumplex.valence']]
+X_test = test_data[['id', 'activity', 'call', 'circumplex.arousal', 'circumplex.valence']]
 X_test['id'] = X_test['id'].str[-2:]
 
-y_train = train_data['mood'].round()
-y_test = test_data['mood'].round()
+y_train = train_data['next_mood'].round()
+y_test = test_data['next_mood'].round()
+
+# Identify unique labels in the training set
+train_labels = np.unique(y_train)
+shift = min(train_labels)
+
+# Create a mapping from train labels to consecutive integers
+label_mapping = {label: i for i, label in enumerate(train_labels)}
+
+# Map labels to consecutive integers
+y_train = np.array([label_mapping[label] for label in y_train])
+y_test = np.array([label_mapping[label] if label in label_mapping else -1 for label in y_test])
 
 param_dist = {
     'n_estimators': randint(50, 500),
@@ -31,19 +43,27 @@ log_loss_scorer = make_scorer(log_loss, greater_is_better=False, needs_proba=Tru
 rand_search = RandomizedSearchCV(estimator=rf, param_distributions=param_dist, n_iter=5, cv=2, scoring=log_loss_scorer, random_state=42)
 rand_search.fit(X_train, y_train)
 
-idx_3 = np.where(y_train == 3)[0][0]
-X_test.iloc[-1] = X_train.iloc[idx_3]
-y_test.iloc[-1] = y_train.iloc[idx_3]
-
+class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
+class_weight_dict = dict(enumerate(class_weights))
 best_params = rand_search.best_params_
 best_rf = RandomForestClassifier(**best_params, random_state=42)
 best_rf.fit(X_train, y_train)
 
 y_pred_probability = best_rf.predict_proba(X_test)
-logloss = log_loss(y_test, y_pred_probability)
-y_pred = y_pred_probability.argmax(axis=1)+min(y_test)
+y_pred = [train_labels[i] for i in y_pred_probability.argmax(axis=1)]
+
+# Filter out test samples with missing labels
+valid_indices = np.where(y_test != -1)
+y_test_filtered = y_test[valid_indices]
+y_pred_probability_filtered = y_pred_probability[valid_indices]
+
+logloss = log_loss(y_test_filtered, y_pred_probability_filtered[:, :len(train_labels)], labels=train_labels)
+y_pred = y_pred_probability.argmax(axis=1) + shift
+y_test = y_test + shift
 print('Accuracy:', accuracy_score(y_test, y_pred))
 print('Cross-Entropy Loss:', logloss)
+print('MSE', np.mean((y_test - y_pred) ** 2))
+print('MAE', np.mean(np.abs()))
 
 plt.plot(range(len(y_test)), y_test, color='red', label='Actual Mood', marker='o', linestyle='--', markersize=3)
 plt.plot(range(len(y_pred)), y_pred, color='blue', label='Predicted Mood', marker='o', linestyle='--', markersize=3)
